80c325a19d3850e880b8045fe98a45079b5bcf39
Shapovalov Dmytro <buonviagio@gmail.com>
2023-03-10 18:32:28 +0200

    My first commit Module4.1 Task4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if(first == second){
+            System.out.println(first);
+            System.out.println(second);
+        }else {
+            System.out.println(first > second ? first : second);
+        }
     }
 }
